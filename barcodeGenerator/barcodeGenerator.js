e46c1b0677173b2a28465cb10090b1e96f2abc18
Javi Fernández <jfernandezriverola@gmail.com>
2016-09-01 12:53:24 +0200

    more

--- barcodeGenerator/barcodeGenerator.js
+++ barcodeGenerator/barcodeGenerator.js
@@ -191,6 +191,82 @@ angular.module('barcodeGenerator', []).directive('barcodeGenerator', [function()
                     return(result);
                 }
             },
+            ean: {
+              encoding: [ ["0001101", "0100111", "1110010"],
+                          ["0011001", "0110011", "1100110"],
+                          ["0010011", "0011011", "1101100"],
+                          ["0111101", "0100001", "1000010"],
+                          ["0100011", "0011101", "1011100"],
+                          ["0110001", "0111001", "1001110"],
+                          ["0101111", "0000101", "1010000"],
+                          ["0111011", "0010001", "1000100"],
+                          ["0110111", "0001001", "1001000"],
+                          ["0001011", "0010111", "1110100"] ],
+              first:  ["000000","001011","001101","001110","010011","011001","011100","010101","010110","011010"],
+              getDigit: function(code, type){
+                // Check len (12 for ean13, 7 for ean8)
+                var len = type == "ean8" ? 7 : 12;
+                code = code.substring(0, len);
+                if (code.length != len) return("");
+                // Check each digit is numeric
+                var c;
+                for(var i=0; i<code.length; i++){
+                  c = code.charAt(i);
+                  if ( (c < '0') || (c > '9') ) return("");
+                }
+                // get checksum
+                code = this.compute(code, type);
+
+                // process analyse
+                var result = "101"; // start
+
+                if (type == "ean8"){
+
+                  // process left part
+                  for(var i=0; i<4; i++){
+                    result += this.encoding[barcode.intval(code.charAt(i))][0];
+                  }
+
+                  // center guard bars
+                  result += "01010";
+
+                  // process right part
+                  for(var i=4; i<8; i++){
+                    result += this.encoding[barcode.intval(code.charAt(i))][2];
+                  }
+
+                } else { // ean13
+                  // extract first digit and get sequence
+                  var seq = this.first[ barcode.intval(code.charAt(0)) ];
+
+                  // process left part
+                  for(var i=1; i<7; i++){
+                    result += this.encoding[barcode.intval(code.charAt(i))][ barcode.intval(seq.charAt(i-1)) ];
+                  }
+
+                  // center guard bars
+                  result += "01010";
+
+                  // process right part
+                  for(var i=7; i<13; i++){
+                    result += this.encoding[barcode.intval(code.charAt(i))][ 2 ];
+                  }
+                } // ean13
+
+                result += "101"; // stop
+                return(result);
+              },
+              compute: function (code, type){
+                var len = type == "ean13" ? 12 : 7;
+                code = code.substring(0, len);
+                var sum = 0, odd = true;
+                for(i=code.length-1; i>-1; i--){
+                  sum += (odd ? 3 : 1) * barcode.intval(code.charAt(i));
+                  odd = ! odd;
+                }
+                return(code + ((10 - sum % 10) % 10).toString());
+              }
+            },
             bitStringTo2DArray: function( digit) {//convert a bit string to an array of array of bit char
                 var d = [];
                 d[0] = [];
